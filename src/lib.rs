cc227259e362df8d9131bc64fd4b011ec790f4a6
Joshua C. Randall <jcrandall@alum.mit.edu>
2019-06-15 23:41:42 +0000

    fix error introduced by merge

--- src/lib.rs
+++ src/lib.rs
@@ -366,7 +366,7 @@ impl Connection {
     /// string or if the underlying SQLite open call fails.
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Connection> {
         let flags = OpenFlags::default();
-        Connection::open_with_flags(path, flags, None)
+        Connection::open_with_flags(path, flags)
     }
 
     /// Open a new connection to an in-memory SQLite database.
