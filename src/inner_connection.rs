86ac7ce921633a28f905ab70666af578286ae8c1
Joshua C. Randall <jcrandall@alum.mit.edu>
2020-02-24 20:34:10 +0000

    don't perform threading mode checks on wasm32

--- src/inner_connection.rs
+++ src/inner_connection.rs
@@ -372,9 +372,28 @@ rusqlite was built against SQLite {} but the runtime SQLite version is {}. To fi
     });
 }
 
+#[cfg(not(any(
+    target_arch = "wasm32"
+)))]
 static SQLITE_INIT: Once = Once::new();
+
+#[cfg(not(any(
+    target_arch = "wasm32"
+)))]
 pub static BYPASS_SQLITE_INIT: AtomicBool = AtomicBool::new(false);
 
+// threading mode checks are not necessary (and do not work) on target
+// platforms that do not have threading (such as webassembly)
+#[cfg(any(
+    target_arch = "wasm32"
+))]
+fn ensure_safe_sqlite_threading_mode() -> Result<()> {
+    Ok(())
+}
+
+#[cfg(not(any(
+    target_arch = "wasm32"
+)))]
 fn ensure_safe_sqlite_threading_mode() -> Result<()> {
     // Ensure SQLite was compiled in thredsafe mode.
     if unsafe { ffi::sqlite3_threadsafe() == 0 } {
